292367b9af4daf5d2f3d5a85d47122c127740bc1
Tom Smith <tomossmith86@icloud.com>
2021-09-24 09:33:57 +0000

    Initial Commit / Create Flask App

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug="True")
+
+## CHANGE TO FALSE BEFORE FINAL DEPLOYMENT ##
