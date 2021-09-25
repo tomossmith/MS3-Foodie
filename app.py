1ed6a60d2142a7ca882051f44e2db5b08d29db84
Tom Smith <tomossmith86@icloud.com>
2021-09-25 14:22:41 +0000

    Added background image and created registration form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,12 +22,17 @@ mongo = PyMongo(app)
 
 @app.route("/index")
 def index():
-    return render_template("index.html", index=index)
+    return render_template("index.html")
 
 @app.route("/get_recipes")
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
