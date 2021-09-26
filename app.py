1a111d3ff542f738cb9aebd54d1af8da2159fb53
Tom Smith <tomossmith86@icloud.com>
2021-09-26 18:52:55 +0000

    Added user profile page

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def register():
         # PLACE NEW USER INTO 'SESSION COOKIE'
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -65,7 +66,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # INVALID PASSWORD
                 flash("Incorrect Username and/or Password, Please Try Again")
@@ -78,6 +82,13 @@ def login():
 
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # GET THE USER'S USERNAME FROM THE DATABASE
+    username = mongo.db.users.find_one({
+        "username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
