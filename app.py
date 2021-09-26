1ef2f215ebb35f5a4dea5b9cd06d8be239598da7
Tom Smith <tomossmith86@icloud.com>
2021-09-26 19:02:43 +0000

    included email submission in the app.py registration route

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def register():
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email").lower(),
         }
         mongo.db.users.insert_one(register)
 
