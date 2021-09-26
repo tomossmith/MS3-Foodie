194a5c61a64b2099f05333e05e91b1a3ffc66697
Tom Smith <tomossmith86@icloud.com>
2021-09-26 15:23:38 +0000

    POST function enabled to Registration form and CSS applied to the form and alert

--- app.py
+++ app.py
@@ -32,6 +32,24 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # CHECK IF USERNAME ALREADY EXISTS IN DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Sorry, This Username already exists!")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+        }
+        mongo.db.users.insert_one(register)
+
+        # PLACE NEW USER INTO 'SESSION COOKIE'
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
