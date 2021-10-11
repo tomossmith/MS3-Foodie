7242312cd1e78f0244454f319133e78016ae1493
Tom Smith <tomossmith86@icloud.com>
2021-10-11 10:45:37 +0000

    added the delete category function and included user authentication to pages that require certain user levels

--- app.py
+++ app.py
@@ -220,6 +220,12 @@ def edit_category(category_id):
  
     return render_template("edit_category.html", category=category)
 
+# DELETE CATEGORY
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category Successfully Deleted")
+    return redirect(url_for("get_categories"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
