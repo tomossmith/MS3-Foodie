8c7f7d0ade470049b1e35a02233ec1770f7d4e89
Tom Smith <tomossmith86@icloud.com>
2021-11-17 15:30:46 +0000

    PEP8 compliance for app.py

--- app.py
+++ app.py
@@ -18,30 +18,22 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 # GLOBAL VARIABLES
 @app.context_processor
 def get_categories():
     categories = list(mongo.db.categories.find())
     return dict(categories=categories)
 
+
 # LANDING PAGE
-
-
 @app.route("/")
 def get_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
-# HOME
-
-@app.route("/get_recipes")
-def get_recipes():
-    recipes = list(mongo.db.recipes.find())
-    return render_template("recipes.html", recipes=recipes)
 
 # REGISTER
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -65,9 +57,8 @@ def register():
         return redirect(url_for("my_recipes", username=session["user"]))
     return render_template("register.html")
 
+
 # LOGIN
-
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -96,6 +87,7 @@ def login():
 
     return render_template("login.html")
 
+
 # LOGOUT
 @app.route("/logout")
 def logout():
@@ -104,12 +96,13 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
-# ADMIN FEATURES
 
+# ADMIN FEATURES
 @app.route("/get_admins")
 def get_admins():
     admins = list(mongo.db.admins.find())
     return render_template("admins.html", admins=admins)
+
 
 # ADD ADMINISTRATOR
 @app.route("/add_admin", methods=["GET", "POST"])
@@ -119,10 +112,12 @@ def add_admin():
             "username": request.form.get("admin_username"),
         }
         mongo.db.admins.insert_one(admins)
-        flash("Administrator Was Successfully Added To The Website Access List!")
+        flash(
+            "Administrator Was Successfully Added To The Website Access List!")
         return redirect(url_for("get_admins", admins=admins))
-    
+
     return render_template("add_admin.html")
+
 
 # DELETE ADMIN
 @app.route("/delete_admin/<admin_id>")
@@ -130,6 +125,7 @@ def delete_admin(admin_id):
     mongo.db.admins.remove({"_id": ObjectId(admin_id)})
     flash("Administrator Successfully Deleted")
     return redirect(url_for("get_admins"))
+
 
 # RECIPE SEARCH FUNCTION
 @app.route("/search", methods=["GET", "POST"])
@@ -148,13 +144,16 @@ def my_recipes(username):
     recipes = mongo.db.recipes.find(
         {"submitted_by": user_id})
     count = recipes.count()
-    return render_template("my_recipes.html", username=username, recipes=recipes, count=count)
+    return render_template(
+        "my_recipes.html", username=username, recipes=recipes, count=count)
+
 
 # INDIVIDUAL RECIPE PAGE
 @app.route("/open_recipe/<recipe_id>")
 def open_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({"_id":  ObjectId(recipe_id)})
     return render_template("open_recipe.html", recipe=recipe)
+
 
 # RECIPES BY TYPE
 @app.route("/recipes_by_type/<category_id>")
@@ -169,6 +168,7 @@ def recipes_by_type(category_id):
     count = recipes.count()
     return render_template("recipes_by_type.html", recipes=recipes,
                            category=category, count=count)
+
 
 # ADD RECIPE
 @app.route("/add_recipe", methods=["GET", "POST"])
@@ -193,7 +193,7 @@ def add_recipe():
         mongo.db.recipes.insert_one(recipe)
         flash("Your Recipe Was Successfully Added To The Cookbook!")
         return redirect(url_for("my_recipes", username=session["user"]))
-    
+
     return render_template("add_recipe.html")
 
 
@@ -230,10 +230,11 @@ def delete_recipe(recipe_id):
     flash("Recipe Successfully Deleted")
     return redirect(url_for("my_recipes", username=session["user"]))
 
+
 # CATEGORIES LIST
 @app.route("/get_categories", methods=["GET", "POST"])
 def get_categories():
-# check if admin username exists in db
+    # check if admin username exists in db
     admin_user = mongo.db.admins.find_one({"username": session["user"]})
     if admin_user:
         categories = list(mongo.db.categories.find())
@@ -241,10 +242,11 @@ def get_categories():
 
     return render_template("no_auth.html")
 
+
 # ADD CATEGORY
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-# check if admin username exists in db
+    # check if admin username exists in db
     admin_user = mongo.db.admins.find_one({"username": session["user"]})
     if admin_user:
         if request.method == "POST":
@@ -256,16 +258,18 @@ def add_category():
             flash("The Category Was Successfully Added")
             return redirect(url_for("get_categories"))
         return render_template("add_category.html")
-    
+
     return render_template("no_auth.html")
+
 
 # EDIT CATEGORY
 @app.route("/edit_category/<category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
-# check if admin username exists in db
+    # check if admin username exists in db
     admin_user = mongo.db.admins.find_one({"username": session["user"]})
     if admin_user:
-        category = mongo.db.categories.find_one({"_id":  ObjectId(category_id)})
+        category = mongo.db.categories.find_one(
+            {"_id":  ObjectId(category_id)})
         if request.method == "POST":
             submit = {
                 "category_name": request.form.get("category_name"),
@@ -275,20 +279,22 @@ def edit_category(category_id):
             flash("The Category Was Successfully Updated")
             return redirect(url_for("get_categories"))
         return render_template("edit_category.html", category=category)
-            
+
     return render_template("no_auth.html")
+
 
 # DELETE CATEGORY
 @app.route("/delete_category/<category_id>")
 def delete_category(category_id):
-# check if admin username exists in db
+    # check if admin username exists in db
     admin_user = mongo.db.admins.find_one({"username": session["user"]})
     if admin_user:
         mongo.db.categories.remove({"_id": ObjectId(category_id)})
         flash("Category Successfully Deleted")
         return redirect(url_for("get_categories"))
-    
+
     return render_template("no_auth.html")
+
 
 # ERROR PAGES:
 # https://flask.palletsprojects.com/en/2.0.x/errorhandling/
@@ -301,5 +307,6 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug="True")
+
 
 # CHANGE TO FALSE BEFORE FINAL DEPLOYMENT
