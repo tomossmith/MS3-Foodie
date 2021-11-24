3beddd8271f2cdb60a8804b210750fb942398e3f
Tom Smith <tomossmith86@icloud.com>
2021-11-24 20:43:25 +0000

    fixed an authentication error on the categories page

--- app.py
+++ app.py
@@ -260,12 +260,13 @@ def delete_recipe(recipe_id):
 # CATEGORIES LIST
 @app.route("/get_categories", methods=["GET", "POST"])
 def get_categories():
-    # check if admin username exists in db
-    admin_user = mongo.db.admins.find_one({"username": session["user"]})
-    if admin_user:
-        categories = list(mongo.db.categories.find())
-        return render_template("categories.html", categories=categories)
-
+    if 'username' in session:
+        # check if admin username exists in db
+        admin_user = mongo.db.admins.find_one({"username": session["user"]})
+        if admin_user:
+            categories = list(mongo.db.categories.find())
+            return render_template("categories.html", categories=categories)
+        return render_template("no_auth.html")
     return render_template("no_auth.html")
 
 
